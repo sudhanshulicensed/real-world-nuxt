dd498566e290a83b99dbd463d4fa96d90f693915
Sudhanshu Chaudhary <sudhanshulicensed>
2023-06-07 12:03:41 +0530

    Added, seo to different pages

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,20 @@
     <h1>Create an Event</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Create an event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Yyou can create a new event'
+        }
+      ]
+    }
+  }
+}
+</script>
